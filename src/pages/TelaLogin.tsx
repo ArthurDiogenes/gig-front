269d0bc4535af9020d024408e9145dee7e087433
Paulo Luan <pauloluan1409@gmail.com>
2025-02-22 01:03:59 -0300

    add componente/ui 'Button' e layout da tela de login 85% concluido e responsivo

--- src/pages/TelaLogin.tsx
+++ src/pages/TelaLogin.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import InputComponent from '../ui/InputComponent'; 
 
 import styles from './TelaLogin.module.css';
+import Button from '../ui/Button';
 
 export default function TelaLogin(){
 
@@ -11,24 +12,27 @@ export default function TelaLogin(){
 
     return(
         <div className={styles.container}>
-            <section className={styles.sectionImg}><img className={styles.img} src="../../public/images/img-login.jpg" alt="" /></section> 
+            <section className={styles.sectionImg}><img className={styles.img} src="/images/img-login.jpg" alt="" /></section> 
                    
             <section className={styles.sectionForm}>
-                <h1 className={styles.titulo}>login</h1>
-                <form action="">
-                    <InputComponent 
-                    type='email'
-                    name='email'
-                    placeholder='Digite seu email'
-                    onClick={clique}
-                    />
-                    <InputComponent
-                    type='password'
-                    name='password'
-                    placeholder='Digite sua senha'
-                    onClick={clique}
-                    />
-                </form>
+                <div className={styles.login}>
+                    <h1 className={styles.title}>login</h1>
+                    <form action="">
+                        <InputComponent 
+                        type='email'
+                        name='email'
+                        placeholder='Digite seu email'
+                        onClick={clique}
+                        />
+                        <InputComponent
+                        type='password'
+                        name='password'
+                        placeholder='Digite sua senha'
+                        onClick={clique}
+                        />
+                    </form>
+                    <Button type='submit'>Entrar</Button>
+                </div>
             </section>
         </div>
     )
