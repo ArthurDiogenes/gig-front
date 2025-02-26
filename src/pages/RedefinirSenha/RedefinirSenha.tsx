5a2c13746edbc487afd5eefe1a51fc03138de9f2
Arthur Diogenes <arthurdfleite@gmail.com>
2025-02-26 16:47:58 -0300

    adicionando validações da tela de redefinir senha

--- src/pages/RedefinirSenha/RedefinirSenha.tsx
+++ src/pages/RedefinirSenha/RedefinirSenha.tsx
@@ -1,38 +1,56 @@
 import InputComponent from "../../ui/InputComponent/InputComponent";
-// Removi a imagem
-
 import styles from "./RedefinirSenha.module.css";
 import Button from "../../ui/Button/Button";
+import { useState } from "react";
 
 export default function RedefinirSenha() {
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  const validatePassword = (password: string) => {
+    return password.length >= 6;
+  };
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    console.log("Solicitação de redefinição de senha enviada");
+    
+    if (!validatePassword(password)) {
+      setError("A senha deve ter pelo menos 6 caracteres.");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setError("As senhas não coincidem.");
+      return;
+    }
+
+    setError("");
+    console.log("Senha redefinida com sucesso");
   };
 
   return (
     <div className={styles.container}>
-      {/* Removida a seção da imagem */}
       <section className={styles.sectionForm}>
         <div className={styles.login}>
           <h1 className={styles.title}>Redefinir Senha</h1>
           <form onSubmit={handleSubmit}>
-
-          <h4 className={styles.subtitulo}>Defina aqui sua senha de acesso à plataforma</h4>
+            <h4 className={styles.subtitulo}>Defina aqui sua senha de acesso à plataforma</h4>
             <InputComponent
-              type="email"
-              name="email"
+              type="password"
+              name="password"
               placeholder="Digite sua nova senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
-
             <InputComponent
-              type="email"
-              name="email"
+              type="password"
+              name="confirmPassword"
               placeholder="Confirme sua senha"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
             />
-
-
-
+            {error && <p className={styles.error}>{error}</p>}
             <Button type="submit">Cadastrar senha</Button>
           </form>
         </div>
