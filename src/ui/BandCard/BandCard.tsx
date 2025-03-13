69068d1a554fe38bcaf7b3a205ebba6a17fa918e
Arthur Diógenes <arthurdfleite@gmail.com>
2025-03-13 16:12:43 -0300

    Melhorando a interface da Home page e adicionando imagens

--- src/ui/BandCard/BandCard.tsx
+++ src/ui/BandCard/BandCard.tsx
@@ -4,16 +4,17 @@ interface BandCardProps {
     name: string;
     genre: string;
     image: string;
+    onClick?: () => void;
 }
 
-export default function BandCard({ name, genre, image }: BandCardProps) {
+export default function BandCard({ name, genre, image, onClick }: BandCardProps) {
     return (
-        <div className={styles.bandCard}>
+        <button className={styles.bandCard} onClick={onClick}>
             <img src={image} alt={name} className={styles.image} />
             <div className={styles.info}>
                 <h3>{name}</h3>
                 <p>{genre}</p>
             </div>
-        </div>
+        </button>
     );
 }
--- src/ui/Navbar/Navbar.tsx
+++ src/ui/Navbar/Navbar.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
     return (
         <nav className={styles.navbar}>
-            <div className={styles.logo}>gig</div>
+            <img src='/images/logo-gig 2.svg' alt='Gig Logo' className={styles.logo} />
             <input type="text" className={styles.search} placeholder="Pesquisar" />
             <div className={styles.menuIcon}>☰</div>
         </nav>
